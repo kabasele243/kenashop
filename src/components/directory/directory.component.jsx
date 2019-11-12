f3da745f7dc97431a309c941dc4a4d946c2d4f70
Kabasele243 <fkabasele7@gmail.com>
2019-11-12 15:44:04 -0500

    update our menu-items with withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,29 +13,34 @@ class Directory extends React.Component {
         {
           title: 'hats',
           imageUrl: 'https://images.unsplash.com/photo-1485675067348-b5ac01cfc282?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
-          id: 1
+          id: 1,
+          linkUrl: 'hats'
         },
         {
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2
+          id: 2,
+          linkUrl: 'jackets'
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3
+          id: 3,
+          linkUrl: 'sneakers'
         },
         {
           title: 'womens',
           imageUrl: 'https://images.unsplash.com/photo-1520880178538-52017968d5d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
           size: 'large',
-          id: 4
+          id: 4,
+          linkUrl: 'womens'
         },
         {
           title: 'mens',
           imageUrl: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=426&q=80',
           size: 'large',
-          id: 5
+          id: 5,
+          linkUrl: 'mens'
         }
       ]
     };
@@ -46,12 +51,13 @@ class Directory extends React.Component {
     //We map through our sections from state
     //We destructure the value we need like { title, imageUrl, id, size} to access them directly
     //We access them through MenuItem That we give state, and it receive them as a props.
+    //We use spread to destructure them using otherSectionsProps so with can add more attribute without expressely naming them.
 
     return (
       <div className='directory-menu'>
 
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionsProps }) => (
+          <MenuItem key={id} {...otherSectionsProps} />
         ))}
 
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
 
 
 
 // MenuItem receive title, imageUrl and Size as props from his parent directory and use it.
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div
       className='background-image'
       style={{
-        backgroundImage: `url(${imageUrl})`
+        backgroundImage: `url(${imageUrl}${linkUrl})`
       }}
     />
     <div className='content'>
@@ -21,4 +21,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
