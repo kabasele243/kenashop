6508b7c72f2e7ac6598ff62afff0a4b0fd45540e
Kabasele243 <fkabasele7@gmail.com>
2019-11-21 13:19:44 -0500

    hidden checkout dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,10 +4,11 @@ import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { createStructuredSelector } from 'reselect';
 import { withRouter } from 'react-router-dom';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {
@@ -22,9 +23,12 @@ const CartDropdown = ({ cartItems, history }) => (
 
             }
         </div>
-        <CustomButton onClick={() => history.push('./checkout')}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => {
+            history.push('./checkout');
+            dispatch(toggleCartHidden())
+        }}>GO TO CHECKOUT</CustomButton>
 
-    </div>
+    </div >
 )
 
 const mapStateToProps = createStructuredSelector({
