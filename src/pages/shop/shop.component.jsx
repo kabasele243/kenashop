57465af8f80d941750c631988231842290f6be5b
Kabasele243 <fkabasele7@gmail.com>
2019-11-25 18:39:10 -0500

    collection routing and selector

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CategoryPage from '../category/category.component';
+import CollectionPage from '../collections/collections.component';
 
 
 const ShopPage = ({ match }) => (
     <div className='shop-page'>
         <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route psth={`${match.path}/:categoryId`} component={CategoryPage} />
-
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>);
 
 
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,5 +1,13 @@
 import { createSelector } from 'reselect';
 
+const COLLECTION_ID_MAP = {
+    hats: 1,
+    sneakers: 2,
+    jackets: 3,
+    women: 4,
+    men: 5,
+    technology: 6,
+}
 
 const selectShop = state => state.shop;
 
@@ -9,3 +17,5 @@ export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 )
+
+export const selectCollection = collectionUrlParam => createSelector([selectCollections], collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]))
